137ea727f0f2ec33a46d99543059d5644208352d
Donosa Matei- Cristian <matei.donosa@gmail.com>
2025-10-02 21:08:23 +0300

    Update responses.py

--- Chatbot/Designed chatbot/chatbot/responses.py
+++ Chatbot/Designed chatbot/chatbot/responses.py
@@ -1,6 +1,6 @@
 import random
 
-R_Eating = ["I don't like eating.", "I run on code, not food!", "Bots don't need to eat 😄"]
+R_Eating = ["I don't like eating.", "I run on code, not food!", "Bots don't need to eat "]
 
 def get_custom_response(topic):
     if topic == "eat":
@@ -11,7 +11,7 @@ def unknown():
     responses = [
         "Could you please rephrase that?",
         "Hmm... I didn't quite get that.",
-        "What does that mean? 🤔",
+        "What does that mean? ",
         "I'm not sure I understand."
     ]
     return random.choice(responses)
