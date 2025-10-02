12f36b572628e657a7565aa61f6f5acff0d5578c
Donosa Matei- Cristian <matei.donosa@gmail.com>
2025-10-02 21:06:08 +0300

    Update probability.py

--- Chatbot/Console chatbot/probability.py
+++ Chatbot/Console chatbot/probability.py
@@ -3,7 +3,7 @@ from responses import get_custom_response, unknown
 RULES = [
     {
         "keywords": ["hello", "hi", "hey", "salut"],
-        "response": "Hello! 😊",
+        "response": "Hello! ",
         "single_response": True
     },
     {
@@ -19,7 +19,7 @@ RULES = [
     {
         "keywords": ["i", "love", "code", "palace"],
         "required": ["code"],
-        "response": "Thank you! ❤️"
+        "response": "Thank you! "
     },
     {
         "keywords": ["what", "you", "eat", "like"],
@@ -28,7 +28,7 @@ RULES = [
     },
     {
         "keywords": ["bye", "goodbye", "see"],
-        "response": "Goodbye! 👋",
+        "response": "Goodbye! ",
         "single_response": True
     },
     {
@@ -38,12 +38,12 @@ RULES = [
     },
     {
         "keywords": ["joke", "funny"],
-        "response": "Why don't programmers like nature? It has too many bugs! 😂",
+        "response": "Why don't programmers like nature? It has too many bugs! ",
         "single_response": True
     },
     {
         "keywords": ["weather", "forecast"],
-        "response": "I can't check the weather, but I hope it's nice where you are! ☀️",
+        "response": "I can't check the weather, but I hope it's nice where you are! ",
         "single_response": True
     },
     {
@@ -54,14 +54,6 @@ RULES = [
 ]
 
 def message_probability(user_message, keywords, single_response=False, required=[]):
-
-    #TODO: Calculează probabilitatea mesajului message_certainty
-    #pt fiecare cuvant din mesaj care apare in recognised_words
-    #message_certainty este incrementat
-    
-    #TODO: Calculează match_ratio ca raportul dintre message_certainty și numărul de cuvinte din keywords
-    #dacă keywords este gol, setăm match_ratio la 0
-    
     
     if required:
         if not all(word in user_message for word in required):
@@ -78,30 +70,6 @@ def check_all_messages(message):
     for rule in RULES:
         None
         
-        #TODO: Calculează probabilitatea mesajului pentru fiecare regulă
-        #folosind funcția message_probability definită mai sus
-        
-        #TODO: dacă prob este mai mare decât highest_prob,
-        # actualizează best_response și highest_prob
-        
-        
-    #TODO: returneaza raspunsul, fie cel de eroare, fie cel gasit 
-    
 
 def get_response(user_input):
     None
-    #TODO: Verifică dacă user_input este gol sau conține doar spații
-
-    #TODO: apeleaza functia split pentru a împărți mesajul în cuvinte 
-    
-    # apoi returneaza rezultatul obtinut folosind check_all_messages pentru a verifica mesajul
-
-# Ce inseamna \s+|[,;?.-]\s*?
-# \s+ înseamnă unul sau mai multe spații albe (inclusiv tab-uri și linii noi)
-# | este operatorul "sau" în expresiile regulate
-# [,;?.-] înseamnă oricare dintre caracterele specificate (
-# virgulă, punct și virgulă, punct, semn de întrebare sau cratimă)
-# \s* înseamnă zero sau mai multe spații albe după aceste caractere
-# deci, expresia întreagă împarte mesajul în cuvinte folosind spațiile albe și semnele de punctuație specificate
-# de exemplu, "Hello, world! How are you?" va fi împărțit în
-# ["hello", "world", "how", "are", "you"]
